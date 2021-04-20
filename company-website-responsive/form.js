df2e0cc0f62b54a8192e9d2d6217794baa54abc4
Ibrahim-amalitech <ibrahim.osuman@amalitech.org>
2021-04-19 17:08:51 -0700

    assignment files

---
+++ company-website-responsive/form.js
@@ -0,0 +1,7 @@
+function display()
+{
+var x=document.details.fname.value;
+var y=document.details.lname.value;
+var z=document.details.phnum.value;
+alert("FIRST NAME:"+x+"     "+"LAST NAME:"+y+"     "+"PHONE NUMBER:"+z);
+}
